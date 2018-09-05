67ca5edef94026a0219b247815d26d054d1c51b7
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-05 10:12:58 -0400

    adding yet another struct for category bank :) (simple hard-coded array for now)

---
+++ HackDay-ReviewApp/CategoryBank.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryBank.swift
+//  HackDay-ReviewApp
+//
+//  Created by DetroitLabs on 9/5/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+struct CategoryBank {
+    
+    var categories = ["Week 1", "Week 2", "Week 3"]
+    
+}
