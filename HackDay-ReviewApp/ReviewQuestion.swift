b1406b83789110b72aaf10b5332b565bf66d1f31
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-05 09:47:16 -0400

    creating ReviewQuestion structure

---
+++ HackDay-ReviewApp/ReviewQuestion.swift
@@ -0,0 +1,16 @@
+//
+//  ReviewQuestion.swift
+//  HackDay-ReviewApp
+//
+//  Created by DetroitLabs on 9/5/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+struct ReviewQuestion {
+    
+    let question: String
+    let answer: String
+    
+}
