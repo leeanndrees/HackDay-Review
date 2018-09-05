b0f24188304ba7c99398a18c747479c99a3cd37e
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-05 13:31:59 -0400

    showing basic question in ReviewViewController

--- HackDay-ReviewApp/ReviewViewController.swift
+++ HackDay-ReviewApp/ReviewViewController.swift
@@ -13,15 +13,22 @@ class ReviewViewController: UIViewController {
     var questionsToShow: [ReviewQuestion] = []
     let qb = QuestionBank()
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         questionsToShow = qb.generateQuestionBank()
+        showQuestion()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func showQuestion() {
+        questionLabel.text = questionsToShow[0].question
     }
 
 
